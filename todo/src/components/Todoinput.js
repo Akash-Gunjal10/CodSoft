f6accd4e1543624e025e2440883b7429d61ea3ce
Akash-Gunjal10 <GUNJALAKASH757@GMAIL.COM>
2024-07-24 17:42:59 +0530

    Add three projects to Codsoft repository

---
+++ todo/src/components/Todoinput.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function Todoinput(prop) {
+    const [inputtask, setInputtask]=useState("")
+  return (
+    <div>
+        <input type='text' name='t1' value={inputtask} onChange={(e)=>setInputtask(e.target.value)}/>
+        <button onClick={()=>{prop.addtodo(inputtask)  
+        setInputtask("")
+       
+        }}>ADD</button>
+    </div>
+  )
+}
+
+export default Todoinput
